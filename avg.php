23872136a3334f82b6868699bccfc0f7b5d677ab
nzzab <wrx88572@student.wroclaw.merito.pl>
2026-04-11 14:47:28 +0200

    Inne wagi 7>8, 3>2

--- avg.php
+++ avg.php
@@ -1,1 +1,11 @@
-srednia wazona
+<?php
+function srednia($a, $b) {
+    return ($a + $b) / 2;
+}
+function srednia_wazona($a, $b) {
+    $waga1 = 8;
+    $waga2 = 2;
+    
+    return ($a * $waga1 + $b * $waga2) / ($waga1 + $waga2);
+}
+
